9cde7b1791e5a26ce87bc9f100f96009d124d9a5
Chris Pall <chrispall@gmail.com>
2024-11-25 16:29:22 -0500

    wip

--- src/main/java/frc/robot/examples/commands/CmdExamples.java
+++ src/main/java/frc/robot/examples/commands/CmdExamples.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import static edu.wpi.first.wpilibj2.command.Commands.print;
 /**
--- src/main/java/frc/robot/examples/leds/LedDisplay.java
+++ src/main/java/frc/robot/examples/leds/LedDisplay.java
@@ -3,7 +3,6 @@ package frc.robot.examples.leds;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj2.command.Commands;
 
 public class LedDisplay {
     
